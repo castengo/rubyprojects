27e68609ea4bb273929a7d04b9abf8653a4b3736
castengo <catalina.astengo@hotmail.com>
2016-09-13 16:44:05 -0600

    Change logo and try to fix jumbotron background

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -4,24 +4,24 @@ class ColorsController < ApplicationController
 	def index
 
 		case params[:product_type]
-		when "Eyes" 
+		when "Eyes"
 	      @shades = Color.joins(:products).where(:products => { :product_type => ["Eye Shadow Palette", "Eye Shadow Single"]} ).order(:l)
-	    when "Lips" 
-	    	@shades = Color.joins(:products).where(:products => { :product_type => "Liptstick" } ).order(:l)	
+	    when "Lips"
+	    	@shades = Color.joins(:products).where(:products => { :product_type => "Lipstick" } ).order(:l)
 	      # Lipstick
-	    when "Face" 
-
+	    when "Face"
+				# Blushes
 	    when nil
 	    	@colors = Color.all.order(:l)
-			@red_yellow_colors = Color.where(h: 8..60).order(:l)
-			@green_colors = Color.where(h: 61..156).order(:l, :s, :h)
-			@blue_colors = Color.where(h: 157..252).order(:l, :s, :h)
-			@purple_pink_colors = Color.where(h: 253..324).order(:l, :s, :h)
-			@pink_red_colors = Color.where(h: [325..360] + [1..7]).order(:l, :s, :h)
+				@red_yellow_colors = Color.where(h: 8..60).order(:l)
+				@green_colors = Color.where(h: 61..156).order(:l, :s, :h)
+				@blue_colors = Color.where(h: 157..252).order(:l, :s, :h)
+				@purple_pink_colors = Color.where(h: 253..324).order(:l, :s, :h)
+				@pink_red_colors = Color.where(h: [325..360] + [1..7]).order(:l, :s, :h)
 	    end
 
 	    respond_to do |format|
-	      format.html 
+	      format.html
 	      format.js
 	    end
 
@@ -32,7 +32,7 @@ class ColorsController < ApplicationController
 		@close_call_colors = Color.close_colors(@color,30,20,10).where.not(id: @spot_on_colors.pluck(:id)).limit(9)
 	end
 
-	private 
+	private
 
 	def set_color
 		@color = Color.find(params[:id])
